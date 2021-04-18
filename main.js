4cbf05eb9c415768994cf1a0acb9a712f28c6624
Lenovo <ihardaronin@gmail.com>
2021-04-18 14:36:45 +0300

    added two homeworks one by one, changed the palindrome function, fixed comments

---
+++ main.js
@@ -0,0 +1,89 @@
+let user = 'John Doe';
+console.log(user);
+let student = 'Ihar';
+console.log(student);
+user = student;
+// 'Ihar'
+console.log(user);
+
+let test = 1;
+console.log(test);
+
+test = test + 1;
+console.log(test);
+
+test = test + '1';
+// 21
+console.log(test);
+
+test = test - 1;
+// 20
+console.log(test);
+
+test = !!test;
+// true
+console.log(test);
+
+const array1 = [2, 3, 5, 8];
+let result = 1;
+for (i = 0; i < array1.length; i++) {
+    result *= array1[i];
+}
+console.log(result);
+
+const array2 = [2, 5, 8, 15, 0, 6, 20, 3];
+for (i = 0; i < array2.length; i++) {
+    if (array2[i] > 5 && array2[i] < 10) {
+        console.log(array2[i]);
+    }
+}
+
+const array3 = [2, 5, 8, 15, 0, 6, 20, 3];
+for (i = 0; i < array3.length; i++) {
+    if (array3[i] % 2 === 0) {
+        console.log(array3[i]);
+    }
+}
+
+// Homework lesson-3
+
+function palindrome(str) {
+    let result = '';
+    for (let i = 0; i < str.length; i++) {
+        result = str[i] + result;
+    }
+    return str === result;
+}
+console.log(palindrome('шалаш'))
+
+
+function min(a,b) {
+    if (a > b) {
+        return b;
+    }
+    return a;
+}
+console.log(min(5,6));
+
+let min = (a,b) => a > b ? b : a;
+
+
+function max(a,y) {
+    if (a > y) {
+        return a;
+    }
+    return y;
+}
+console.log(max(5,6))
+
+let max = (a,y) => a > y ? a : y;
+
+
+const array = Array.from({length:10}, () => Math.floor(Math.random() * 100));
+for (let i = 0; i < array.length; i++) {
+    if (array[i] % 10 === 0) {
+        array[i] = array[i].toString();
+        array[i] = array[i].replaceAll('0','zero');
+    }
+}
+console.log(array)
