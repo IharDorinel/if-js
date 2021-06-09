1d32c7860f7addec26e4b7d48d5c7699f7aa46c0
Lenovo <ihardaronin@gmail.com>
2021-06-09 18:07:42 +0300

    nothing changed

--- lesson_12/main.js
+++ lesson_12/main.js
@@ -1,9 +1,9 @@
 const homeGuestContentDIv = document.querySelector('#home-guest-content');
 
 fetch('https://fe-student-api.herokuapp.com/api/hotels/popular')
-  .then((response) => response.json())
-  .then((data) => data.forEach((elem) => {
-    homeGuestContentDIv.innerHTML += `
+    .then((response) => response.json())
+    .then((data) => data.forEach((elem) => {
+        homeGuestContentDIv.innerHTML += `
         <div class="home-guests-box box">
           <a href="#" class="home-guests-photo-link">
             <img class="home-guests-photo photo" src=${elem.imageUrl} alt="hotel_leopold">
@@ -12,6 +12,7 @@ fetch('https://fe-student-api.herokuapp.com/api/hotels/popular')
           <p class="home-destination home-text"><a href="#">${elem.city}, ${elem.country}</a></p>
         </div>
     `;
-  }))
-  .then((data) => console.log(data))
-  .catch((err) => console.log('This is error', err));
+    }))
+    .then((data) => console.log(data))
+    .catch((err) => console.log('This is error', err));
+
