8f7f15ee74bf2f55f625c064203b362b100bd8e1
Lenovo <ihardaronin@gmail.com>
2021-06-13 10:04:55 +0300

    deleted headers with application-json

--- lesson_14/main.js
+++ lesson_14/main.js
@@ -1,15 +1,10 @@
-const sendBtn = document.getElementById('sendBtn');
 const formEl = document.getElementById('formEl');
-
 
 formEl.addEventListener('submit', async event => {
     event.preventDefault();
     const formData = new FormData(formEl);
     const response = await fetch('https://fe-student-api.herokuapp.com/api/file', {
         method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
         body: formData,
     })
     const responseJSON = await response.json();
