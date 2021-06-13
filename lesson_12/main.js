7c2fa21813a0d5f3560fee9c1a5fd657291b4e7b
Lenovo <ihardaronin@gmail.com>
2021-06-13 11:24:05 +0300

    added variable

--- lesson_12/main.js
+++ lesson_12/main.js
@@ -1,9 +1,11 @@
 const homeGuestContentDIv = document.querySelector('#home-guest-content');
+const fetchData = [];
 
-fetch('https://fe-student-api.herokuapp.com/api/hotels/popular')
-    .then((response) => response.json())
-    .then((data) => data.forEach((elem) => {
-        homeGuestContentDIv.innerHTML += `
+    fetch('https://fe-student-api.herokuapp.com/api/hotels/popular')
+        .then((response) => response.json())
+        .then((data) => fetchData.push(data))
+        .then((data) => fetchData.forEach((elem) => {
+            homeGuestContentDIv.innerHTML += `
         <div class="home-guests-box box">
           <a href="#" class="home-guests-photo-link">
             <img class="home-guests-photo photo" src=${elem.imageUrl} alt="hotel_leopold">
@@ -12,7 +14,7 @@ fetch('https://fe-student-api.herokuapp.com/api/hotels/popular')
           <p class="home-destination home-text"><a href="#">${elem.city}, ${elem.country}</a></p>
         </div>
     `;
-    }))
-    .then((data) => console.log(data))
-    .catch((err) => console.log('This is error', err));
+        }))
+        .then((data) => console.log(data))
+        .catch((err) => console.log('This is error', err));
 
